f19924beba48343f444b0cb61c83490658f42bda
Vincent Damewood <git@vdamewood.com>
2019-02-22 04:40:53 -0800

    Add tags and posts to model

---
+++ migrations/0001_initial.py
@@ -0,0 +1,33 @@
+import datetime
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=50)),
+                ('slug', models.SlugField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=50)),
+                ('slug', models.SlugField()),
+                ('created', models.DateField(default=datetime.date.today)),
+                ('edited', models.DateField(auto_now=True)),
+                ('content', models.TextField()),
+                ('tags', models.ManyToManyField(to='loki.Tag')),
+            ],
+        ),
+    ]
---
+++ models.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Vincent Damewood
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import date
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    created = models.DateField(default=date.today)
+    edited = models.DateField(auto_now=True)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
